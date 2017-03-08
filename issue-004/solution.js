c7c38875a7eb89bd6c56d6dc237a0de45ff69988
dessHub <deskip95@gmail.com>
2017-03-08 23:03:36 +0300

    solved drill 1.4

--- issue-004/solution.js
+++ issue-004/solution.js
@@ -1,3 +1,21 @@
 function grade(a) {
     // implement your solution here
+    if(a >= 0 && a < 30){
+      return 'F'
+    }
+    else if(a >= 30 && a <40){
+      return 'E'
+    }
+    else if(a >= 40 && a <50){
+      return 'D'
+    }
+    else if(a >= 50 && a <60){
+      return 'C'
+    }
+    else if(a >= 60 && a <80){
+      return 'B'
+    }
+    else if(a >= 80 && a <=100){
+      return 'A'
+    }
 }
