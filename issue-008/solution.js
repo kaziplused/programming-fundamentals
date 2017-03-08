0ab7584d45c46e017b13be21b640398d935b7300
dessHub <deskip95@gmail.com>
2017-03-09 01:27:44 +0300

    solved drill 1.8

--- issue-008/solution.js
+++ issue-008/solution.js
@@ -1,4 +1,14 @@
 // implement your solution here
-function sum() {
+function sum(arg) {
+  var sum = 0;
+  var val = Array.prototype.slice.call(arguments, sum.length);
+ for(i=0; i<val.length; i++){
+     sum += val[i];
+
+ }
+return sum;
 
 }
+
+
+console.log(sum(1,2,2));
