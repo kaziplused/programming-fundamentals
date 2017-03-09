7755cee9d79bca97ec60f7f391f113d40af12c29
FelixIvance <felixrunye@gmail.com>
2017-03-09 16:16:00 +0300

    drill 1.6 - Select Sum

--- issue-006/solution.js
+++ issue-006/solution.js
@@ -1,4 +1,11 @@
 // implement your solution here
-function selectSum() {
+function selectSum(myArray) {
+	var total = 0;
+     for (var i in myArray) {
+         if (typeof a[i] == 'number') {
+             total += a[i];
+         }
+     }
+    return total;
 
 }
